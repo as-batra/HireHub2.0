491cb75c03fa5aaf77ba6a937dc5d7d5c1343e57
Arshjot Singh <143347763+as-batra@users.noreply.github.com>
2026-07-13 11:24:56 +0530

    Fix CORS for Vercel

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://hirehub-snowy-rho.vercel.app"],
   credentials: true,
 };
 app.use(cors(corsOptions));
