f2cf6c03682918b570a18e5ce7d00a069ad86ae1
Arshjot Singh <143347763+as-batra@users.noreply.github.com>
2026-08-20 00:47:02 +0530

    fixed error in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import store from "./redux/store.js";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
-const persistor = persistStowre(store);
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
